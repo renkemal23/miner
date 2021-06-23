bfa740db775b47515d4ff63c8dbe6385c21aeb3e
renkemal23 <84149346+renkemal23@users.noreply.github.com>
2021-06-23 11:34:23 -0600

    Create xmr.sh

---
+++ xmr.sh
@@ -0,0 +1,5 @@
+# xmrcoin
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-static-x64.tar.gz
+xmrig-6.12.2-linux-static-x64.tar.gz
+./xmrig -o pool.supportxmr.com:443 -u 87TanpskS8cVWPvUAky6VTFv2iA49ucmpFwkrEKD3r7XBxep2dTUS8E5gqnjuQpNVEAoC2ryL7eQZhSbZTcdM28h5jmP8CB -k --tls -p kemal23
